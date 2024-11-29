73604f74c148e8924a3cd46fc9f0bafab9579f40
Angel Nieves <nievesaj@gmail.com>
2024-11-29 18:17:50 -0500

    chore: Remove Tailwind configuration

--- next.config.js
+++ next.config.js
@@ -4,9 +4,8 @@ const nextConfig = {
   compiler: {
     emotion: true
   },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true
   }
 };
 
